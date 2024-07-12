0b93c05362f5c3ce7c4de176bdf1120faead7da3
arin <dwiariniayuambarwati@gmail.com>
2024-07-12 12:11:01 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+            "./desain.html",
+
+],
+  theme: {
+    extend: {
+
+      fontFamily: {
+        body: ['Anton']
+
+      }
+    },
+  },
+  plugins: [],
+}
+
